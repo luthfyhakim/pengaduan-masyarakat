805cdfe37431699736d835c99e558da48bbdb4b3
iki_wahyu <wahyu.220117rpl0205@gmail.com>
2022-06-18 13:13:45 +0700

    add users controller and routing to pages

---
+++ app/Http/Controllers/UsersUserController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Masyarakat;
+use App\Models\Pengaduan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UsersUserController extends Controller
+
+{
+    public function index()
+    {
+        return view('user.landing');
+    }
+
+    public function login(Request $request)
+    {
+
+        //kita mengecek ditabel masyarakat apakah udah ada pada tabel
+        $username = Masyarakat::where('username', $request->username)->first();
+
+        if (!$username) {
+            return redirect()->back()->with(['pesan' => 'Username tidak terdaftar']);
+        }
+
+        $password = Hash::check($request->password, $username->password);
+
+        if (!$password) {
+            return redirect()->back()->with(['pesan' => 'Password tidak sesuai']);
+        }
+
+        if (Auth::guard('masyarakat')->attempt(['username' => $request->username, 'password' => $request->password])) {
+            return redirect()->back();
+        } else {
+            return redirect()->back()->with(['pesan' => 'Akun tidak terdaftar!']);
+        }
+    }
+
+    //mengarahkan ke halaman register  
+
+    public function formRegister()
+    {
+        return view('user.register');
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->all();
+
+        $validate = Validator::make($data, [
+            'nik' => ['required'],
+            'nama' => ['required'],
+            'username' => ['required'],
+            'password' => ['required'],
+            'telp' => ['required'],
+        ]);
+
+        if ($validate->fails()) {
+            return redirect()->back()->with(['pesan' => $validate->errors()]);
+        }
+
+        $username = Masyarakat::where('username', $request->username)->first();
+
+        if ($username) {
+            return redirect()->back()->with(['pesan' => 'Username sudah terdaftar']);
+        }
+
+        Masyarakat::create([
+            'nik' => $data['nik'],
+            'nama' => $data['nama'],
+            'username' => $data['username'],
+            'password' => Hash::make($data['password']),
+            'telp' => $data['telp'],
+        ]);
+
+        return redirect()->route('pekat.index');
+    }
+
+    public function logout()
+    {
+        Auth::guard('masyarakat')->logout();
+
+        return redirect()->back();
+    }
+
+    public function storePengaduan(Request $request)
+    {
+        if (!Auth::guard('masyarakat')->user()) {
+            return redirect()->back()->with(['pesan' => 'Login dibutuhkan!'])->withInput();
+        }
+
+        $data = $request->all();
+
+        $validate = Validator::make($data, [
+            'isi_laporan' => ['required'],
+        ]);
+
+        if ($validate->fails()) {
+            return redirect()->back()->withInput()->withErrors($validate);
+        }
+
+        //jika foto berhasil dikirm akan dipindah kan oleh pengkondisian ini 
+        if ($request->file('foto')) {
+            $data['foto'] = $request->file('foto')->store('assets/pengaduan', 'public');
+        }
+
+        date_default_timezone_set('Asia/Bangkok');
+
+        $pengaduan = Pengaduan::create([
+            'tgl_pengaduan' => date('Y-m-d h:i:s'),
+            'nik' => Auth::guard('masyarakat')->user()->nik,
+            'isi_laporan' => $data['isi_laporan'],
+            'foto' => $data['foto'] ?? '',
+            'status' => '0',
+        ]);
+
+        if ($pengaduan) {
+            return redirect()->route('pekat.laporan', 'me')->with(['pengaduan' => 'Berhasil terkirim!', 'type' => 'success']);
+        } else {
+            return redirect()->back()->with(['pengaduan' => 'Gagal terkirim!', 'type' => 'danger']);
+        }
+    }
+
+    public function laporan($siapa = '')
+    {
+        $terverifikasi = Pengaduan::where([['nik', Auth::guard('masyarakat')->user()->nik], ['status', '!=', '0']])->get()->count();
+        $proses = Pengaduan::where([['nik', Auth::guard('masyarakat')->user()->nik], ['status', 'proses']])->get()->count();
+        $selesai = Pengaduan::where([['nik', Auth::guard('masyarakat')->user()->nik], ['status', 'selesai']])->get()->count();
+
+        $hitung = [$terverifikasi, $proses, $selesai];
+
+        if ($siapa == 'me') {
+            $pengaduan = Pengaduan::where('nik', Auth::guard('masyarakat')->user()->nik)->orderBy('tgl_pengaduan', 'desc')->get();
+
+            return view('user.laporan', ['pengaduan' => $pengaduan, 'hitung' => $hitung, 'siapa' => $siapa]);
+        } else {
+            $pengaduan = Pengaduan::where([['nik', '!=', Auth::guard('masyarakat')->user()->nik], ['status', '!=', '0']])->orderBy('tgl_pengaduan', 'desc')->get();
+
+            return view('user.laporan', ['pengaduan' => $pengaduan, 'hitung' => $hitung, 'siapa' => $siapa]);
+        }
+    }
+}
