1a01af0b587371fd6a2fdbd1e6ccb063fd939e2b
iki_wahyu <wahyu.220117rpl0205@gmail.com>
2022-06-20 10:32:55 +0700

    create model petugas & user admin(masalah =>  admin tidak terdaftar)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //buat user admin/petugas
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+         \App\Models\Petugas::create([
+            'nama_petugas' => 'admin',
+            'username' => 'wahyu',
+            'password' => Hash::make('1'),
+            'telp' => '0822665073626',
+            'level' => 'admin',
+         ]);
     }
 }
