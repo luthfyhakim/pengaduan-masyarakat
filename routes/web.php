cc8db96bafeca461e087a6659e05a696234ff406
iki_wahyu <wahyu.220117rpl0205@gmail.com>
2022-06-20 10:31:02 +0700

    create routing for from login admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\User\UserController;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,3 +30,12 @@ Route::post('/store', [UserController::class, 'storePengaduan'])->name('pekat.st
 Route::get('/laporan/{siapa?}', [UserController::class, 'laporan'])->name('pekat.laporan');
 
 Route::get('/logout', [UserController::class, 'logout'])->name('pekat.logout');
+
+Route::prefix('admin')->group(function() {
+    
+    Route::get('/', [AdminController::class, 'formLogin'])->name('admin.formLogin');
+    Route::post('/login', [AdminController::class, 'login'])->name('admin.login');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+});
