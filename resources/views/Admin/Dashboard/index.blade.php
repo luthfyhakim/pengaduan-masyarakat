9463b0f9af79bb09106fd39bf535d6b42728b9b0
luthfyhakim <luthfyhakim250404@gmail.com>
2022-06-20 15:57:59 +0700

    update halaman

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HALAMAN DASHBOARD</title>
-</head>
-<body>
+@extends('layouts.admin')
 
-    <h1>hello world</h1>
-    
-</body>
-</html>
+@section('title', 'Halaman Dashboard')
+
+@section('header', 'Dashboard')
