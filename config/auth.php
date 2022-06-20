e3b01999d1c14c21bc49e1896141686669f14171
iki_wahyu <wahyu.220117rpl0205@gmail.com>
2022-06-20 10:29:16 +0700

    create guard&providers admin

--- config/auth.php
+++ config/auth.php
@@ -44,6 +44,10 @@ return [
             'driver' => 'session',
             'provider' => 'masyarakats',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -76,6 +80,10 @@ return [
         'masyarakats' => [
             'driver' => 'eloquent',
             'model' => App\Models\Masyarakat::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Petugas::class,
         ],
 
         // 'users' => [
