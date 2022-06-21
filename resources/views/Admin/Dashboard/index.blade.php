61313af200c3a84bdeb7049ecc962772392f93b5
luthfyhakim <luthfyhakim250404@gmail.com>
2022-06-21 11:41:56 +0700

    update halaman index dashboard

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -3,3 +3,48 @@
 @section('title', 'Halaman Dashboard')
 
 @section('header', 'Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Petugas</div>
+                <div class="card-body">
+                    <div class="text-center">
+                        {{ $petugas }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Masyarakat</div>
+                <div class="card-body">
+                    <div class="text-center">
+                        {{ $masyarakat }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Pengaduan Proses</div>
+                <div class="card-body">
+                    <div class="text-center">
+                        {{ $proses }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">Pengaduan Selesai</div>
+                <div class="card-body">
+                    <div class="text-center">
+                        {{ $selesai }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
