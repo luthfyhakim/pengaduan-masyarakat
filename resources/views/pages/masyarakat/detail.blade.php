43b21dea18d4cc3faa9e97373337edf2b633698b
iki_wahyu <wahyu.220117rpl0205@gmail.com>
2022-07-05 08:49:01 +0700

    fix bug

--- resources/views/pages/masyarakat/detail.blade.php
+++ resources/views/pages/masyarakat/detail.blade.php
@@ -6,7 +6,7 @@ Data Pengaduan
 @section('content')
 <main class="h-full pb-16 overflow-y-auto">
   <div class="container grid px-6 mx-auto">
-    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+    <h2 class="my-6 text-2xl font-semibold text-white">
       Data Pengaduan
     </h2>
 
--- resources/views/pages/masyarakat/index.blade.php
+++ resources/views/pages/masyarakat/index.blade.php
@@ -107,14 +107,14 @@ Dashboard
 
 
 
+{{-- 
 
 
 
-
-{{-- <main class="h-full pb-16 overflow-y-auto">
+<main class="h-full pb-16 overflow-y-auto">
 
   <div class="container px-6 mx-auto grid">
-    <h2 class="my-6 text-2xl font-semibold text-center text-gray-700 dark:text-gray-200">
+    <h2 class="my-6 text-2xl font-semibold text-center text-gray-700 dark:text-gray-200 text-white">
       Silahkan ajukan pengaduan Anda!
     </h2>
 
